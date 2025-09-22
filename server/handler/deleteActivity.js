36a3abc94398bff057088015f5ed1c24466166a4
Tukaram <tukatodkari@gmail.com>
2025-09-23 03:18:46 +0530

    console changes

--- server/handler/deleteActivity.js
+++ server/handler/deleteActivity.js
@@ -2,7 +2,7 @@ const DateEvent = require('../models/DateEvent');
 
 async function deleteActivity(req, res) {
   try {
-    console.log("Request Body for Deletion:", req.body);
+    console.log("Request Body for Deletion:", req.query);
     let { id, date } = req.query;
     if (!id || !date) {
       return res.status(400).json({ message: 'ID and date are required' });
@@ -14,16 +14,19 @@ async function deleteActivity(req, res) {
     if (!deletedEvent) {
       return res.status(404).json({ message: 'Event not found' });
     }
+    console.log("Deleted Event:", deletedEvent);
+
+    // Fetch and return updated list of events for the date
 
     await DateEvent.find({ date: date })
-    .then((data) => {
-      console.log("=====data=====", data);
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      console.error('Error fetching date journey:', err);
-      res.status(500).json({ message: 'Error fetching date journey', error: err.message });
-    });
+      .then((data) => {
+        console.log("___received_data___");
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        console.error('Error fetching date journey:', err);
+        res.status(500).json({ message: 'Error fetching date journey', error: err.message });
+      });
 
   } catch (err) {
     console.error('Error deleting event:', err);
